f96f212fe9645eb24fc4ffeb0cee5522e8075284
Franklin Ndlovu <franklin.ndlovu@gmail.com>
2022-08-26 13:01:24 +0200

    Add project files.

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,100 @@
+﻿public class BinaryTreea
+{
+    public Node Root { get; set; }
+
+    // TODO :Add Node
+
+    public Node Remove(int data)
+    {
+        if (Root == null)
+            return null;
+        var parentNode = Root;
+        var currentNode = Root;
+        while(currentNode != null && currentNode.Data != data)
+        {
+            parentNode = currentNode;
+            if (currentNode.Data > data)
+                currentNode = currentNode.LeftNode;
+            else
+                currentNode = currentNode.RightNode;
+        }
+
+        if (currentNode == null)
+            return null;
+        // Leaf Node
+        if(currentNode.LeftNode ==null && currentNode.RightNode==null)
+        {
+            if (currentNode == parentNode.LeftNode)
+                parentNode.LeftNode = null;
+            else
+                parentNode.RightNode = null;
+
+        }
+        //Parent with Right Child
+        else if (currentNode.LeftNode == null)
+        {
+            if (currentNode == parentNode.LeftNode)
+                parentNode.LeftNode = currentNode.RightNode;
+            else
+                parentNode.RightNode = currentNode.RightNode;
+        }
+        //Parent with Left Child
+        else if (currentNode.RightNode == null)
+        {
+            if (currentNode == parentNode.LeftNode)
+                parentNode.LeftNode = currentNode.LeftNode;
+            else
+                parentNode.RightNode = currentNode.LeftNode;
+        }
+        //Parent with both children
+        else
+        {
+            var nextChildNode = currentNode.RightNode;
+            while (nextChildNode.LeftNode != null)
+            {
+                nextChildNode = nextChildNode.LeftNode;
+            }
+            if ()
+        }
+        return currentNode;
+
+    }
+
+
+    public void Add(Node node)
+    {
+        if (Root == null)
+        {
+            Root = node;
+            return;
+        }
+        var parentNode = Root;
+        var currentNode = Root;
+        while (currentNode !=null)
+        {
+            parentNode = currentNode;
+            if (currentNode.Data > node.Data)
+            {
+                if(currentNode.LeftNode == null)
+                {
+                    parentNode.LeftNode = node;
+                    return;
+                }
+                currentNode = currentNode.LeftNode;
+            }
+            else
+            {
+                if (currentNode.RightNode == null)
+                {
+                    parentNode.RightNode = node;
+                    return;
+                }
+                currentNode = currentNode.RightNode;
+            }
+                
+        }
+            
+    }
+}
+
+
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// FizzBuzz
+// Loop through from 0 to 100
+//for each number check if its a multiple of 3 and return Fizz if it
+//if it's a multiple of 5 display Buzz
+// if it's a multiple of both display FizzBuzz
+// otherwise display the number
+
+//for (int number = 0; number <= 100; number++)
+//{
+//    if (number % 3 == 0 && number % 5 == 0)
+//        Console.WriteLine("fizzbuzz");
+//    else if (number % 3 == 0)
+//        console.writeline("fizz");
+//    else if (number % 5 == 0)
+//        console.writeline("buzz");
+//    else
+//        console.writeline(number);
+//}
+
+var binaryTree = new BinaryTreea();
+
+binaryTree.Add(new Node(8));
+binaryTree.Add(new Node(4));
+binaryTree.Add(new Node(12));
+binaryTree.Add(new Node(2));
+binaryTree.Add(new Node(6));
+binaryTree.Add(new Node(10));
+binaryTree.Add(new Node(14));
+binaryTree.Add(new Node(9));
+var removedNode = binaryTree.Remove(8);
+var notFound = "Node not found";
+Console.WriteLine($"{(removedNode == null ? notFound : removedNode.Data)}");
+
+
+
+
